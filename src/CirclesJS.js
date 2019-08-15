0f859f3b136d415092c605d716779d9e47c598f5
SuicV <drissoulhbibcontact@gmail.com>
2019-08-15 13:10:15 +0100

    [remove] config attribute

--- src/CirclesJS.js
+++ src/CirclesJS.js
@@ -12,21 +12,21 @@ export default class Circles {
     exist(['el', 'radius', 'value'], { el: conf.el, ...conf.circle })
 
     // SETTING DEFAULTS CONFIGURATION
-    this.config.circle = setDefaultCofnig(['stroke_width', 'stroke_color',
+    this.circle = setDefaultCofnig(['stroke_width', 'stroke_color',
       'stroke_linecap'], conf.circle)
 
     if (typeof conf.middleText !== 'undefined') {
-      this.config.middleText = setDefaultCofnig(['font_size', 'font_weight',
+      this.middleText = setDefaultCofnig(['font_size', 'font_weight',
         'color', 'font_family'], conf.middleText)
     }
 
     if (typeof conf.fillRest === 'object') {
-      this.config.fillRest = { ...this.config.circle, ...this.config.fillRest }
-      typeof this.config.circle.max_value === 'undefined' ? this.config.fillRest.value = 100 : this.config.fillRest.value = this.config.circle.max_value
+      this.fillRest = { ...this.circle, ...this.fillRest }
+      typeof this.circle.max_value === 'undefined' ? this.fillRest.value = 100 : this.fillRest.value = this.circle.max_value
     }
 
     this.el = conf.el
-    this.config.middle = getMiddleCoord(conf.el)
+    this.middle = getMiddleCoord(conf.el)
 
     this.isDrawed = false
   }
@@ -35,14 +35,14 @@ export default class Circles {
     this.isDrawed = true
 
     let fillRestPath, text
-    const circlePath = createCircle({ ...this.config.circle, ...this.config.middle })
+    const circlePath = createCircle({ ...this.circle, ...this.middle })
 
-    if (typeof this.config.fillRest !== 'undefined') {
-      fillRestPath = createCircle({ ...this.config.fillRest, ...this.config.middle })
+    if (typeof this.fillRest !== 'undefined') {
+      fillRestPath = createCircle({ ...this.fillRest, ...this.middle })
       this.el.appendChild(fillRestPath)
     }
-    if (typeof this.config.middleText !== 'undefined') {
-      text = createText({ ...this.config.middleText, ...this.config.middle }, this.config.circle.value)
+    if (typeof this.middleText !== 'undefined') {
+      text = createText({ ...this.middleText, ...this.middle }, this.circle.value)
       this.el.appendChild(text)
     }
 
@@ -56,9 +56,9 @@ export default class Circles {
 
   animate (duration = 500) {
     const step = getAnimationStep(duration)
-    const to = this.config.circle.value
-    this.config.circle.value = 0
+    const to = this.circle.value
+    this.circle.value = 0
     this.draw()
-    window.requestAnimationFrame(fillCircleAnimation.bind(this, duration, this.config.circle.value, to, 0, step))
+    window.requestAnimationFrame(fillCircleAnimation.bind(this, duration, this.circle.value, to, 0, step))
   }
 }
--- src/helpers/Animation.js
+++ src/helpers/Animation.js
@@ -11,18 +11,18 @@ export function getAnimationStep (duration) {
   }
 }
 export function fillCircleAnimation (duration, from, to, currantTime, step) {
-  if (currantTime <= duration || this.config.circle.value <= to) {
-    const angle = valueToAngle(this.config.circle.value,
-      this.config.circle.max_angle, this.config.circle.max_value)
+  if (currantTime <= duration || this.circle.value <= to) {
+    const angle = valueToAngle(this.circle.value,
+      this.circle.max_angle, this.circle.max_value)
     // GETTING d ATTRIBUTE AND CHANGE IT
-    const dAttribute = getCirclePath(angle, { ...this.config.circle, ...this.config.middle })
+    const dAttribute = getCirclePath(angle, { ...this.circle, ...this.middle })
     this.elContent.circle.setAttribute('d', dAttribute)
 
     // ANIMATE VALUE JUST WHEN MIDDLE VALUE ISN'T A TEXT
-    if (typeof this.config.middleText !== 'undefined' && typeof this.config.middleText.value_type !== 'undefined') {
-      this.elContent.middleText.textContent = getTextValue(this.config.circle.value, this.config.middleText.value_type)
+    if (typeof this.middleText !== 'undefined' && typeof this.middleText.value_type !== 'undefined') {
+      this.elContent.middleText.textContent = getTextValue(this.circle.value, this.middleText.value_type)
     }
-    this.config.circle.value = parseInt(linearAnimation(duration, from, to, currantTime))
+    this.circle.value = parseInt(linearAnimation(duration, from, to, currantTime))
     currantTime += step
     window.requestAnimationFrame(fillCircleAnimation.bind(this, duration, from, to, currantTime, step))
   }
