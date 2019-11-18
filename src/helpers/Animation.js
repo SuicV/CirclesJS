6f524e1939caa86695ee98ae49e7b7eb67affc37
SuicV <drissoulhbibcontact@gmail.com>
2019-11-18 19:17:52 +0100

    [fix] animation bug

--- src/helpers/Animation.js
+++ src/helpers/Animation.js
@@ -1,13 +1,31 @@
 import { valueToAngle } from './Maths'
 import { getCirclePath, getTextValue } from './SVGHelper'
+
+/**
+ * function return value to draw in a given time
+ * @param {int} duration duration of animation
+ * @param {int} from start value
+ * @param {int} to end value
+ * @param {float} time current time
+ */
 function linearAnimation (duration, from, to, time) {
-  return (time * ((to - from) / duration) + from).toFixed(2)
+  return (time * ((to - from) / duration) + from)
 }
+/**
+ * function to animate filling of cirlce
+ * @param {float} time current time
+ * @param {int} duration duration of animation
+ * @param {int} from start value
+ * @param {int} to end value
+ * @this {Cirlcle} this reference to Circle class
+ */
 export function fillCircleAnimation (time, duration, from, to) {
   const currentTime = +new Date()
-  if (currentTime <= time + duration) {
+  if (currentTime <= time + duration || to > this.circle.value) {
     const angle = valueToAngle(this.circle.value,
       this.circle.max_angle, this.circle.max_value)
+    // GET VALUE TO DRAW
+    this.circle.value = parseInt(linearAnimation(duration, from, to, currentTime - time))
     // GETTING d ATTRIBUTE AND CHANGE IT
     const dAttribute = getCirclePath(angle, { ...this.circle, ...this.middle })
     this.elContent.circle.setAttribute('d', dAttribute)
@@ -16,7 +34,6 @@ export function fillCircleAnimation (time, duration, from, to) {
     if (typeof this.middleText !== 'undefined' && typeof this.middleText.value_type !== 'undefined') {
       this.elContent.middleText.textContent = getTextValue(this.circle.value, this.middleText.value_type)
     }
-    this.circle.value = linearAnimation(duration, from, to, currentTime - time)
     window.requestAnimationFrame(fillCircleAnimation.bind(this, time, duration, from, to))
   }
 }
